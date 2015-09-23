befd6ffbdccaf3d7c2ae74ec6accc72c0cc3dadc
Chew Yung Chung <A0133662@u.nus.edu>
2015-09-23 18:00:39 +0800

    Updated Logic,LogicController and LogicExecutor classes to reflect using of stub classes for independent testing

---
+++ src/Logic/LogicExecutor.java
@@ -0,0 +1,25 @@
+package Logic;
+
+public class LogicExecutor {
+	StorageStub mainStorage;
+	
+	public static CommandResults handleAdd(CommandStub command) {
+		return new CommandResults();
+	}
+	
+	public static CommandResults handleEdit(CommandStub command) {
+		return new CommandResults();
+	}
+	
+	public static CommandResults handleDelete(CommandStub command) {
+		return new CommandResults();
+	}
+	
+	public static CommandResults handleView(CommandStub command) {
+		return new CommandResults();
+	}
+	
+	public static CommandResults handleInvalid(CommandStub command) {
+		return new CommandResults();
+	}
+}
