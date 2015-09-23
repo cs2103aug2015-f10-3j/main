89effe76853b93621322cf95b79ad9fb17042704
Juliana Seng <hexcone@gmail.com>
2015-09-23 21:14:49 +0800

    Add retrieveTasks method stub

--- src/storage/StorageController.java
+++ src/storage/StorageController.java
@@ -1,7 +1,13 @@
 package storage;
 
+import java.util.ArrayList;
+
 public class StorageController {
 	public int retrieveNumberofTasks() {
 		return 0;
+	}
+	
+	public ArrayList<Task> retrieveTasks(String fileName) {
+		return null;
 	}
 }
