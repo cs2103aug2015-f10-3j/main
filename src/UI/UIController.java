38e33fa539a3d122cba61516225952cf11895721
AngKiangSiang <ks.dynasty@gmail.com>
2015-09-23 20:48:35 +0800

    Stub method for integration meeting

--- src/UI/UIController.java
+++ src/UI/UIController.java
@@ -4,4 +4,12 @@ public class UIController {
 	public UIController(){
 		
 	}
+	
+	public String[] processUserInput(String input){
+		String[] output =null;
+		
+		//Call logicApi to process input
+		
+		return output;
+	}
 }
