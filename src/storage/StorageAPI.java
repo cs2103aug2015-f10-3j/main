4245384eb0c97b669e1486bd676133a1870e4381
Juliana Seng <hexcone@gmail.com>
2015-09-23 22:18:15 +0800

    Add storage method stubs

---
+++ src/storage/StorageAPI.java
@@ -0,0 +1,17 @@
+package storage;
+
+import java.util.ArrayList;
+
+public class StorageAPI {
+	public int retrieveNumberofTasks() {
+		return 0;
+	}
+	
+	public ArrayList retrieveTasks(String fileName) {
+		return null;
+	}
+	
+	public boolean storeTasks(String filename, ArrayList taskList) {
+		return false;
+	}
+}
---
+++ src/storage/StorageDataParser.java
@@ -0,0 +1,27 @@
+package storage;
+
+import java.util.ArrayList;
+
+import com.sun.xml.internal.txw2.Document;
+
+public class StorageDataParser {
+	public int retrieveNumberofTasks() {
+		return 0;
+	}
+	
+	public ArrayList retrieveTasks(String fileName) {
+		return null;
+	}
+	
+	public boolean storeTasks(String filename, ArrayList taskList) {
+		return false;
+	}
+	
+	public Document retrieveXmlFile(String fileName) {
+		return null;
+	}
+	
+	public boolean writeXmlFile(ArrayList taskList) {
+		return false;
+	}
+}
