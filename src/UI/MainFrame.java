87a0ab197bc278ed72a490d04596e7b3c50d719d
AngKiangSiang <ks.dynasty@gmail.com>
2015-09-23 20:49:33 +0800

    UI MainFrame and CommandLinePanel created

---
+++ src/UI/MainFrame.java
@@ -0,0 +1,51 @@
+package UI;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class MainFrame {
+    /**
+     * Create the GUI and show it.  For thread safety,
+     * this method should be invoked from the
+     * event-dispatching thread.
+     */
+	private static final String TITLE = "PaddleTask";
+	
+    private static void createAndShowGUI() {
+        //Create and set up the window.
+        //Use the Java look and feel.
+        try {
+            UIManager.setLookAndFeel(
+                UIManager.getCrossPlatformLookAndFeelClassName());
+        } catch (Exception e) { }
+
+        //Make sure we have nice window decorations.
+        JFrame.setDefaultLookAndFeelDecorated(true);
+        JDialog.setDefaultLookAndFeelDecorated(true);
+        
+        JFrame frame = new JFrame(TITLE);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JLabel emptyLabel = new JLabel("Welcome");
+        emptyLabel.setPreferredSize(new Dimension(800, 800));
+        CommandLinePanel panel = new CommandLinePanel();
+        panel.populateContentPane(frame.getContentPane());
+       // frame.getContentPane().add(emptyLabel, BorderLayout.CENTER);
+
+        //Display the window.
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        //Schedule a job for the event-dispatching thread:
+        //creating and showing this application's GUI.
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
+}
