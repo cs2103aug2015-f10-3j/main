b5905ad6a14d987645be2c277b25bc66f6682399
chewyungchung <gaiaboy_92@hotmail.com>
2015-09-25 23:15:27 +0800

    Removed the stub classes

--- src/logic/Logic.java
+++ src/logic/Logic.java
@@ -1,9 +1,43 @@
 package logic;
 
+import java.util.ArrayList;
+import parser.Command;
+import logic.data.Task;
+
 public class Logic {
-	private static LogicController mainController = new LogicController();
 	
-	public static CommandResults processCommand(String userInput) {
-		return mainController.parseCommand(userInput);
+	private ArrayList<Command> commandHistory;
+	private ArrayList<Command> undoHistory;
+	
+	public Logic() {
+		
 	}
+	
+	public static ArrayList<Task> processCommand(String userInput) {
+		return null;
+	}
+	
+	private ArrayList<Task> parseCommand(String userInput) {
+		return null;
+	}
+	
+	private ArrayList<Task> executeCommand(Command cmd) {
+		return null;
+	}
+	
+	private void appendToCommandHistory(Command cmd) {
+		commandHistory.add(cmd);
+	}
+	
+	private void appendToUndoHistory(Command cmd) {
+		undoHistory.add(cmd);
+	}
+	
+	private Command getMostRecentCommand() {
+		return commandHistory.get(commandHistory.size()-1);
+	}
+}
+
+class CommandResults {
+	
 }
