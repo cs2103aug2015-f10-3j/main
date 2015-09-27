ff31954250fdf292d5be2d7d0f3438b3c37ccc3b
Juliana Seng <hexcone@gmail.com>
2015-09-28 02:06:50 +0800

    Add parseTask(), writeXml()

--- src/storage/StorageDataParser.java
+++ src/storage/StorageDataParser.java
@@ -1,6 +1,7 @@
 package storage;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -8,6 +9,14 @@ import java.util.ArrayList;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
@@ -55,7 +64,7 @@ public class StorageDataParser {
             Document doc = parseXml();
             
             //~ System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
-            NodeList nodeList = doc.getElementsByTagName("task");
+            NodeList nodeList = doc.getElementsByTagName("item");
             
             for (int temp = 0; temp < nodeList.getLength(); temp++) {
                 Node nNode = nodeList.item(temp);
@@ -64,7 +73,7 @@ public class StorageDataParser {
                 if (nNode.getNodeType() == Node.ELEMENT_NODE) {
                     Element eElement = (Element) nNode;
                     DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
-                    String taskId = eElement.getAttribute("taskId");
+                    String taskId = eElement.getElementsByTagName("taskId").item(0).getTextContent();
                     int taskId_int = Integer.valueOf(taskId);
                     String description = eElement.getElementsByTagName("description").item(0).getTextContent();
                     String createdAt = eElement.getElementsByTagName("createdAt").item(0).getTextContent();
@@ -118,11 +127,96 @@ public class StorageDataParser {
      * This method returns a string representation of the XML version of the Task objects
      * 
      * @param  task  an ArrayList of all Task objects
-     * @return        a string representation of the XML file
+     * @return       a Document representing the XML document
      */
-    protected String parseTasks(ArrayList<Task> taskList) {
-        //TODO
-        return null;
+    protected Document parseTask(ArrayList<Task> taskList) {
+        Document doc = null;
+        try {
+
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            doc = dBuilder.newDocument();
+            
+            Element root = doc.createElement("task");
+            doc.appendChild(root);
+            
+            for (Task task : taskList) {
+                Element item = doc.createElement("item");
+                root.appendChild(item);
+                
+                // taskId
+                Element taskId = doc.createElement("taskId");
+                taskId.appendChild(doc.createTextNode(Integer.toString(task.getTaskId())));
+                
+                // description
+                Element description = doc.createElement("description");
+                description.appendChild(doc.createTextNode(task.getDescription()));
+                
+                // createdAt
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+                String formattedDateTime = task.getCreatedAt().format(formatter);
+                Element createdAt = doc.createElement("createdAt");
+                createdAt.appendChild(doc.createTextNode(formattedDateTime));
+                
+                // type
+                Element type = doc.createElement("type");
+                type.appendChild(doc.createTextNode(task.getType()));
+                
+                Element start;
+                Element end;
+                Element complete;
+                switch (task.getType()) {
+                    case "floating":
+                        // start
+                        start = doc.createElement("start");
+                        start.appendChild(doc.createTextNode(""));
+                        
+                        // end
+                        end = doc.createElement("end");
+                        end.appendChild(doc.createTextNode(""));
+                        
+                        // complete
+                        complete = doc.createElement("complete");
+                        complete.appendChild(doc.createTextNode(""));
+                        break;
+                    case "timed":
+                        //start
+                        start = doc.createElement("start");
+                        formattedDateTime = ((TimedTask) task).getStart().format(formatter);
+                        start.appendChild(doc.createTextNode(""));
+                        
+                        // end
+                        end = doc.createElement("end");
+                        formattedDateTime = ((TimedTask) task).getEnd().format(formatter);
+                        end.appendChild(doc.createTextNode(formattedDateTime));
+                        
+                        // complete
+                        complete = doc.createElement("complete");
+                        complete.appendChild(doc.createTextNode(""));
+                        break;
+                    case "deadline":
+                        // start
+                        start = doc.createElement("start");
+                        start.appendChild(doc.createTextNode(""));
+                        
+                        // end
+                        end = doc.createElement("end");
+                        formattedDateTime = ((DeadlineTask) task).getEnd().format(formatter);
+                        end.appendChild(doc.createTextNode(formattedDateTime));
+                        
+                        // complete
+                        complete = doc.createElement("complete");
+                        boolean complete_bool = Boolean.valueOf(((DeadlineTask) task).isComplete());
+                        complete.appendChild(doc.createTextNode(""));
+                        break;
+                    default:
+                        break;
+                }
+            }
+        } catch (ParserConfigurationException e) {
+            System.out.println("Error building document");
+        }
+        return doc;
     }
     
     /**
@@ -149,12 +243,40 @@ public class StorageDataParser {
     /**
      * This method writes all Task objects to file
      * 
-     * @param  string  string representation of the XML file
-     * @return       <code>true</code> if the tasks are successfully stored; 
-     *               <code>false</code> otherwise.
+     * @param  doc  a Document representing the XML document
+     * @return      <code>true</code> if the tasks are successfully stored; 
+     *              <code>false</code> otherwise.
      */
-    protected boolean writeXml(ArrayList<Task> taskList) {
-        //TODO
-        return false;
+    protected boolean writeXml(Document doc) {
+        try {
+            // Save the document to the disk file
+            TransformerFactory tranFactory = TransformerFactory.newInstance();
+            Transformer aTransformer = tranFactory.newTransformer();
+
+            // format the XML nicely
+            aTransformer.setOutputProperty(OutputKeys.ENCODING, "ISO-8859-1");
+
+            aTransformer.setOutputProperty(
+                    "{http://xml.apache.org/xslt}indent-amount", "4");
+            aTransformer.setOutputProperty(OutputKeys.INDENT, "yes");
+
+            DOMSource source = new DOMSource(doc);
+            
+            FileWriter fos = new FileWriter(FILE_NAME);
+            StreamResult result = new StreamResult(fos);
+            aTransformer.transform(source, result);
+
+        } catch (IOException e) {
+            System.out.println("Error writing to file");
+            return false;
+        } catch (TransformerConfigurationException e1) {
+            System.out.println("Error outputting document");
+            return false;
+        } catch (TransformerException e) {
+            System.out.println("Error initializing transformer");
+            return false;
+        }   
+        
+        return true;
     }
 }
