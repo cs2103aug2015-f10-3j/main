e9360cf5bc486655afc1318bb1d8a8ca8650c521
Juliana Seng <hexcone@gmail.com>
2015-09-28 05:55:26 +0800

    Add StorageControllerTest stubs

---
+++ src/storage/StorageControllerTest.java
@@ -0,0 +1,58 @@
+package storage;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class StorageControllerTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void testAddTask() {
+        fail("Not yet implemented");
+    }
+    
+    @Test
+    public void testViewTask() {
+        fail("Not yet implemented");
+    }
+    
+    @Test
+    public void testViewTaskString() {
+        fail("Not yet implemented");
+    }
+    
+    @Test
+    public void testViewTaskInt() {
+        fail("Not yet implemented");
+    }
+    
+    @Test
+    public void testUpdateTask() {
+        fail("Not yet implemented");
+    }
+    
+    @Test
+    public void testCompleteTask() {
+        fail("Not yet implemented");
+    }
+    
+    @Test
+    public void testWriteAllToFile() {
+        fail("Not yet implemented");
+    }
+    
+    @Test
+    public void testGetAvailableTaskId() {
+        fail("Not yet implemented");
+    }
+}
