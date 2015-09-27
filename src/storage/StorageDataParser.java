8a353094214a249c6c7a0e92380a9ee84193d425
Juliana Seng <hexcone@gmail.com>
2015-09-27 22:09:14 +0800

    Add readTask(), parseXml(), and sample file

--- src/storage/StorageDataParser.java
+++ src/storage/StorageDataParser.java
@@ -1,10 +1,23 @@
 package storage;
 
+import java.io.File;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 
-import org.w3c.dom.Document;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import storage.data.DeadlineTask;
+import storage.data.FloatingTask;
 import storage.data.Task;
+import storage.data.TimedTask;
 
 
 public class StorageDataParser {
@@ -15,12 +28,20 @@ public class StorageDataParser {
     /**
      * This method check if the XML file exist and create it if not
      * 
-     * @return       <code>true</code> if the task is exist, or is successfully created; 
-     *               <code>false</code> otherwise.
+     * @return       file containing Task objects
      */
-    protected boolean createFileIfNotExist() {
-        //TODO
-        return false;
+    protected File getFile() {
+        File file = new File(FILE_NAME);
+        if (!file.exists()) {
+            // Create file if it does not exist
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+        return file;
     }
     
     /**
@@ -29,8 +50,68 @@ public class StorageDataParser {
      * @return       ArrayList of all Task objects
      */
     protected ArrayList<Task> readTask() {
-        //TODO
-        return null;
+        ArrayList<Task> taskList = new ArrayList<Task>();
+        try {
+            Document doc = parseXml();
+            
+            //~ System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
+            NodeList nodeList = doc.getElementsByTagName("task");
+            
+            for (int temp = 0; temp < nodeList.getLength(); temp++) {
+                Node nNode = nodeList.item(temp);
+                Task task = null;
+                //System.out.println("\nCurrent Element :" + nNode.getNodeName());
+                if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+                    Element eElement = (Element) nNode;
+                    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+                    String taskId = eElement.getAttribute("taskId");
+                    int taskId_int = Integer.valueOf(taskId);
+                    String description = eElement.getElementsByTagName("description").item(0).getTextContent();
+                    String createdAt = eElement.getElementsByTagName("createdAt").item(0).getTextContent();
+                    LocalDateTime createdAt_localdatetime = LocalDateTime.parse(createdAt, formatter);
+                    String type = eElement.getElementsByTagName("type").item(0).getTextContent();
+                    String start = eElement.getElementsByTagName("start").item(0).getTextContent();
+                    LocalDateTime start_localdatetime = LocalDateTime.parse(start, formatter);
+                    String end = eElement.getElementsByTagName("end").item(0).getTextContent();
+                    LocalDateTime end_localdatetime = LocalDateTime.parse(end, formatter);
+                    String complete = eElement.getElementsByTagName("complete").item(0).getTextContent();
+                    boolean complete_boolean = Boolean.valueOf(complete);
+                    
+                    /*
+                    System.out.println("taskId: " + taskId);
+                    System.out.println("description: " + description);
+                    System.out.println("createdAt: " + createdAt);
+                    System.out.println("type: " + type);
+                    System.out.println("start: " + start);
+                    System.out.println("end: " + end);
+                    System.out.println("complete: " + complete);
+                    */
+                    
+                    switch (type) {
+                        case "floating":
+                            task = new FloatingTask(taskId_int, description, createdAt_localdatetime);
+                            break;
+                        case "timed":
+                            task = new TimedTask(taskId_int, description, createdAt_localdatetime, start_localdatetime, end_localdatetime);
+                            break;
+                        case "deadline":
+                            task = new DeadlineTask(taskId_int, description, createdAt_localdatetime, end_localdatetime, complete_boolean);
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                
+                if (task != null) {
+                    taskList.add(task);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+        
+        return taskList;
     }
     
     /**
@@ -50,8 +131,19 @@ public class StorageDataParser {
      * @return       XML Document of Nodes
      */
     protected Document parseXml() {
-        //TODO
-        return null;
+        Document doc = null;
+        try {
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            doc = dBuilder.parse(getFile());
+            
+            // Ensures that the XML DOM view of a document is identical
+            doc.getDocumentElement().normalize();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        return doc;
     }
     
     /**
