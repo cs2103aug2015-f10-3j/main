3af7368fd310899a380d587422713f1c7bf9dbe2
AngKiangSiang <ks.dynasty@gmail.com>
2015-09-30 00:01:54 +0800

    ViewTaskCommand, completed filter by time method.

--- src/input/ViewTaskCommand.java
+++ src/input/ViewTaskCommand.java
@@ -1,11 +1,15 @@
 package input;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 
 import logic.data.Task;
 
 public class ViewTaskCommand implements Command {
 
+	private static final int OFFSET_ZERO = 0;
+	private static final int OFFSET_ONE = 1;
+	
 	@Override
 	public ArrayList<Task> execute() {
 		// TODO Auto-generated method stub
@@ -23,4 +27,38 @@ public class ViewTaskCommand implements Command {
 		return selectedTask;
 	}
 
+
+	private ArrayList<Task> selectTaskByPeriod(ArrayList<Task> allTask, String period){
+		ArrayList<Task> selectedTask = new ArrayList<Task>();
+		LocalDateTime systemTime = LocalDateTime.now();
+		LocalDateTime beforeThisTime = systemTime;
+		period = period.toLowerCase();
+		switch(period){
+		case "today":
+			beforeThisTime = beforeThisTime.withHour(OFFSET_ZERO).withMinute(OFFSET_ZERO).withSecond(OFFSET_ZERO)
+							.withNano(OFFSET_ZERO).plusDays(OFFSET_ONE);
+			break;
+		case "tomorrow":
+			beforeThisTime = beforeThisTime.withHour(OFFSET_ZERO).withMinute(OFFSET_ZERO).withSecond(OFFSET_ZERO)
+							.withNano(OFFSET_ZERO).plusDays(OFFSET_ONE).plusDays(OFFSET_ONE);
+			break;
+		case "week":
+			beforeThisTime = beforeThisTime.withHour(OFFSET_ZERO).withMinute(OFFSET_ZERO).withSecond(OFFSET_ZERO)
+							.withNano(OFFSET_ZERO).plusDays(OFFSET_ONE).plusWeeks(OFFSET_ONE);
+			break;
+		case "month":
+			beforeThisTime = beforeThisTime.withHour(OFFSET_ZERO).withMinute(OFFSET_ZERO).withSecond(OFFSET_ZERO)
+							.withNano(OFFSET_ZERO).plusDays(OFFSET_ONE).plusMonths(OFFSET_ONE);
+			break;
+		}
+		for(Task t : allTask){
+			LocalDateTime deadline = null;
+			//LocalDateTime deadline = t.getEnd();
+			if(deadline.isBefore(beforeThisTime)){
+				selectedTask.add(t);
+			}
+		}
+
+		return selectedTask;
+	}
 }
