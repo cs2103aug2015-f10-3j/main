2f342d848fbf052af0eade5b3e94a74855d4d5ba
AngKiangSiang <ks.dynasty@gmail.com>
2015-09-30 00:08:47 +0800

    Corrected methods for ViewTaskCommand

--- src/input/ViewTaskCommand.java
+++ src/input/ViewTaskCommand.java
@@ -9,14 +9,30 @@ public class ViewTaskCommand implements Command {
 
 	private static final int OFFSET_ZERO = 0;
 	private static final int OFFSET_ONE = 1;
-	
+
 	@Override
 	public ArrayList<Task> execute() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	private ArrayList<Task> selectTasksByType(ArrayList<Task> allTask, Class<?> typeOfTask){
+	private ArrayList<Task> selectTasksByType(ArrayList<Task> allTask, String type){
+		type = type.toLowerCase();
+		Class<?> typeOfTask = null;
+		switch(type){
+		case "floating":
+			//typeOfTask = Task.class;
+			break;
+		case "deadline" :
+			//typeOfTask = Task.class;
+			break;
+		case "timed" :
+			//typeOfTask = Task.class;
+			break;
+		case "all": 
+		default:
+			typeOfTask = Task.class;
+		}
 		ArrayList<Task> selectedTask = new ArrayList<Task>();
 		for(Task t : allTask){
 			if(typeOfTask.isInstance(t)){
@@ -34,22 +50,25 @@ public class ViewTaskCommand implements Command {
 		LocalDateTime beforeThisTime = systemTime;
 		period = period.toLowerCase();
 		switch(period){
-		case "today":
-			beforeThisTime = beforeThisTime.withHour(OFFSET_ZERO).withMinute(OFFSET_ZERO).withSecond(OFFSET_ZERO)
-							.withNano(OFFSET_ZERO).plusDays(OFFSET_ONE);
-			break;
 		case "tomorrow":
 			beforeThisTime = beforeThisTime.withHour(OFFSET_ZERO).withMinute(OFFSET_ZERO).withSecond(OFFSET_ZERO)
 							.withNano(OFFSET_ZERO).plusDays(OFFSET_ONE).plusDays(OFFSET_ONE);
 			break;
+
 		case "week":
 			beforeThisTime = beforeThisTime.withHour(OFFSET_ZERO).withMinute(OFFSET_ZERO).withSecond(OFFSET_ZERO)
 							.withNano(OFFSET_ZERO).plusDays(OFFSET_ONE).plusWeeks(OFFSET_ONE);
 			break;
+
 		case "month":
 			beforeThisTime = beforeThisTime.withHour(OFFSET_ZERO).withMinute(OFFSET_ZERO).withSecond(OFFSET_ZERO)
 							.withNano(OFFSET_ZERO).plusDays(OFFSET_ONE).plusMonths(OFFSET_ONE);
 			break;
+
+		case "today":
+		default:
+			beforeThisTime = beforeThisTime.withHour(OFFSET_ZERO).withMinute(OFFSET_ZERO).withSecond(OFFSET_ZERO)
+							.withNano(OFFSET_ZERO).plusDays(OFFSET_ONE);
 		}
 		for(Task t : allTask){
 			LocalDateTime deadline = null;
