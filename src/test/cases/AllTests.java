158b75cad9c5ac78269619578823e551abf4fdca
Juliana Seng <hexcone@gmail.com>
2015-11-09 22:28:21 +0800

    Add JUnit test suite

---
+++ src/test/cases/AllTests.java
@@ -0,0 +1,16 @@
+//@@author A0126332R
+package test.cases;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ FlowTest.class, TestClearCommand.class,
+        TestCommandParser.class, TestEditTaskCommand.class,
+        TestHelpCommand.class, TestStorageController.class,
+        TestTaskController.class, TestViewCommand.class,
+        ViewTaskCommandTest.class })
+public class AllTests {
+
+}
