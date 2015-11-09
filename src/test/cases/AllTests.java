72cfc5df646d421aa2cbce8dea00b3a7249dd6ab
Joel <imjoeltan@gmail.com>
2015-11-09 23:36:36 +0800

    Update test cases

--- src/test/cases/AllTests.java
+++ src/test/cases/AllTests.java
@@ -6,7 +6,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ FullSystemFlowTest.class, TestClearCommand.class,
+@SuiteClasses({ FullSystemFlowTest.class,
         TestCommandParser.class, TestEditTaskCommand.class,
         TestHelpCommand.class, TestStorageController.class,
         TestTaskController.class, TestViewCommand.class,
